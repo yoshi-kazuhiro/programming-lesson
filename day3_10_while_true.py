f1a0eeb4b22337c7783aff37780c096dcca8aeda
Yoshimura Kazuhiro (吉村 和浩) <Kazuhiro.Yoshimura@jp.ricoh.com>
2025-05-17 16:09:10 +0900

    array、for、whileの例を追加

---
+++ day3_10_while_true.py
@@ -0,0 +1,11 @@
+import time
+count = 0
+
+while True:
+		print(str(count))
+		count = count + 1
+		if (count == 10):
+			break;
+		else:
+			pass
+		time.sleep(1)
---
+++ day3_20_while_loop.py
@@ -0,0 +1,7 @@
+import time
+count = 0
+
+while count < 10:
+		print(str(count))
+		count = count + 1
+		time.sleep(1)
